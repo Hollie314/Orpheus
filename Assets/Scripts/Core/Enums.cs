8bbfcd1b0e85fade04584fad4c3ebad316d8cd48
MontyLois <lois.monty@outlook.fr>
2025-04-10 16:18:50 +0200

    starting ring managment

---
+++ Assets/Scripts/Core/Enums.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+public enum RingSize
+{
+    small = 0, 
+    medium = 1, 
+    large = 2, 
+    extraLarge = 3 
+}
+
---
+++ Assets/Scripts/Core/Rings/Floor.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour
+{
+    public int floorIndex;
+    public float minY;
+    public float maxY;
+    public List<Ring> rings;
+}
