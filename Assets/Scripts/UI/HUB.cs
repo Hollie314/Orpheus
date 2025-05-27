a41d04df32bf57f440880b70fcdfe4c2b6eba3c0
Hollie314 <157151660+Hollie314@users.noreply.github.com>
2025-05-27 20:51:30 +0200

    UI et quelque anneau habiller sans les VFX anim

--- Assets/Scripts/UI/HUB.cs
+++ Assets/Scripts/UI/HUB.cs
@@ -5,7 +5,7 @@ public class HUB : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadSceneAsync(0); 
+        SceneManager.LoadSceneAsync(2); 
     }
 
     public void QuitGame()
